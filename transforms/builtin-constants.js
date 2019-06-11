127e6799c630c0003817a7185ce0eb3e7d13a0a4
ilyaigpetrov <ilyaigpetrov@gmail.com>
2019-06-11 09:19:30 +0500

    Restructure ./vendor to reflect node's fs structure

--- transforms/builtin-constants.js
+++ transforms/builtin-constants.js
@@ -11,6 +11,7 @@ module.exports = function (file) {
         .replace(/primordials/g, 'window')
         .replace(/internalBinding\('config'\)/g, '({})')
         .replace(/internalBinding\('buffer'\)/g, '({ kMaxLength: 1 })'),
+        //.replace(/timeout\.close/g, 'timeout && timeout.close'), // Fix bug in timers shim.
     );
     next();
   });
